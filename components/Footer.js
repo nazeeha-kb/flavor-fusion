bf6e94d7fa12a4324938834afc5749619eaf77ea
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-08-14 19:20:40 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="border-t-1 border-gray-300">
-    <div className="flex justify-between  p-4 text-sm text-gray-600 mx-auto max-w-7xl">
+    <div className="flex sm:flex-row sm:gap-0 gap-2 sm:items-baseline items-center flex-col justify-between  p-4 text-sm text-gray-600 mx-auto max-w-7xl">
       <div className="cursor-pointer hover:text-gray-900">&copy; 2025 . FlavorFusion . All rights reserved</div>
       <div className="flex gap-2 cursor-pointer">
         <div className="hover:text-gray-900">Privacy Policy</div>
