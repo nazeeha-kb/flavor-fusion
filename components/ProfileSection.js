b16111eb64ded4d47a1269b43849cd8e514a060e
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-08-14 19:49:31 +0300

    Update ProfileSection.js

--- components/ProfileSection.js
+++ components/ProfileSection.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const ProfileSection = () => {
   return (
-    <div className="md:w-[60vw] w-[80vw] md:h-[40vh] h-[30vh] bg-gray-400 rounded-md mt-10 text-center pt-32 border-2 border-gray-50 ">
-      <div className="text-white  text-2xl font-bold">
-        Development in progress...
+    <div className="md:w-[60vw] w-[80vw] md:h-[40vh] h-[30vh] bg-gray-300 rounded-lg my-10 border-2 border-gray-50 flex justify-center items-center">
+      <div className="w-[90%] h-[90%] bg-white rounded-lg flex justify-center items-center text-center">
+        <div className="text-gray-700  text-2xl font-bold">
+          Development in progress...
+        </div>
       </div>
     </div>
   );
